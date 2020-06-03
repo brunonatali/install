ffe959ce5a5f1f58fc9c1378437b0b14aa055f84
Bruno Natali <b010010010n@gmail.com>
2020-06-02 21:45:37 -0300

    First commit

---
+++ src/Factory.php
@@ -0,0 +1,229 @@
+<?php declare(strict_types=1);
+
+namespace BrunoNatali\Install;
+
+use BrunoNatali\Tools\OutSystem;
+
+class Install implements InstallInterface
+{
+    private $dir;
+    private $pbin;
+    private $instructions;
+
+    protected $appName;
+
+    Protected $outSystem;
+
+    function __construct($config = [])
+    {
+
+        $config += [
+            "dir" => null,
+            "pbin" => null,
+            "instructions" => null,
+            "outSystemEnabled" => true
+        ];
+
+        $this->dir = $config['dir'];
+
+        if ($this->dir !== null)
+            $this->appName = $this->getAppName();
+        else 
+            $this->appName = null;
+
+        if ($config['pbin'] !== null) 
+            $this->setPBin($config['pbin']);
+        else if ($this->dir !== null)
+            $this->setPBin(); // Apply default
+        else     
+            $this->pbin = null;
+
+        if ($config['instructions'] !== null) 
+            $this->setInstructions($config['instructions']);
+        else if ($this->dir !== null)
+            $this->setInstructions(); // Apply default
+        else     
+            $this->instructions = null;
+
+        $config = OutSystem::helpHandleAppName( 
+            $config,
+            [
+                "outSystemName" => 'Install'
+            ]
+        );
+
+        $this->outSystem = new OutSystem($config);
+    }
+
+    public function setDir(string $dir)
+    {
+        $this->dir = $dir;
+    }
+
+    public function setPBin(string $pbin = null)
+    {
+        if ($pbin !== null)
+            $this->pbin = $pbin;
+        else if ($this->dir !== null) 
+            $this->pbin = $this->dir . '/../pbin'; // Default location
+        else
+            throw new \Exception("Provide bin folder or setDir() first", self::INSTALL_WRONG_PBIN);
+
+        if (!file_exists($this->pbin)) {
+            throw new \Exception("'pbin' folder does not exist on: " . $this->pbin, self::INSTALL_PBIN_ERROR);
+            $this->pbin = null;
+        }
+    }
+
+    public function setInstructions($instructions = false)
+    {
+        if (\is_array($instructions)) {
+            $this->instructions = $instructions;
+        } else if (\is_string($instructions)) {
+            if (!\file_exists($instructions))
+                throw new \Exception("Instructions file not found in: " . $instructions, self::INSTALL_INS_ERROR);
+                 
+            $fileContent = \json_decode(\file_get_contents($instructionsFile), true);
+            if (!\is_array($fileContent))
+                throw new \Exception("Instructions file in wrong format", self::INSTALL_INS_WRONG_FORMAT);
+
+            $this->instructions = $fileContent;
+        } else {
+            if ($this->dir !== null) 
+                $this->setInstructions($this->dir . '/install-instructions.json'); // default location
+            else
+                throw new \Exception("Instructions must be array or .json file path", self::INSTALL_WRONG_INS);
+        }
+    }
+
+    public function getAppName(): string
+    {
+        if ($this->dir === null)
+            throw new \Exception("App name could not be determined before set dir", self::INSTALL_DIR_ERROR);
+
+        \chdir($this->dir . '/..'); // Change to app root dir
+        return \basename(\getcwd()); // Get APP dir name
+    }
+
+    public function install()
+    {
+        $result = 'Nothing to do, check your configuration.';
+
+        if ($this->appName === null)
+            $this->appName = $this->getAppName();
+
+        $this->outSystem->stdout('Starting ' . $this->appName . ' instalation', OutSystem::LEVEL_NOTICE);
+        
+        /**
+         * Make all files in pbin folder executable 
+        */
+        if ($this->pbin !== null) {
+            $this->outSystem->stdout('Make binaries executable:', OutSystem::LEVEL_NOTICE);
+
+            $folderContent = \array_diff(\scandir($this->pbin), ['..', '.']);
+
+            foreach ($folderContent as $bin) {
+                $result = 'Done.';
+
+                $this->outSystem->stdout("\t- $bin", OutSystem::LEVEL_NOTICE);
+                \chmod($this->pbin . '/' . $bin, 0755);
+            }
+        }
+
+        /**
+         * Install binaries to system sbin folder
+        */
+        if (isset($this->instructions['sys-bin-files'])) {
+            if (!\is_array($this->instructions['sys-bin-files']))
+                throw new \Exception("'sys-bin-files' must be array", self::INSTALL_SYS_BIN_WRONG_FORMAT);
+
+            $this->outSystem->stdout('Installing binaries:', OutSystem::LEVEL_NOTICE);
+
+            foreach ($this->instructions['sys-bin-files'] as $bin) {
+                $symLink = '/usr/sbin/' . basename($bin, '.php');
+        
+                if (\file_exists($symLink))
+                    \unlink($symLink);
+        
+                $out = [];
+                $result = $this->run("ln -s " . $this->pbin . "/$bin $symLink", $out);
+        
+                if ($result !== 0)
+                    throw new \Exception("Error while installing '$bin': " . (\is_array($out) ? \implode(PHP_EOL, $out) : $out), self::INSTALL_SYMLINK_ERROR);
+            
+                $this->outSystem->stdout("\t- $bin", OutSystem::LEVEL_NOTICE);
+                $result = 'Done.';
+            }
+        }
+
+        /**
+         * Crate install & configure services
+        */
+        if (isset($this->instructions['service'])) {
+            if (!\is_array($this->instructions['service']))
+                throw new \Exception("'service' must be array", self::INSTALL_SERVICE_WRONG_FORMAT);
+
+            $this->outSystem->stdout('Installing and configuring services:', OutSystem::LEVEL_NOTICE);
+
+            foreach ($this->instructions['service'] as $key => $service) {
+
+                if (!isset($service['name']) || !isset($service['bin'])) 
+                    throw new \Exception("Config error in service index $key", self::INSTALL_SERVICE_CONFIG_ERROR);
+
+                $serviceName = $service['name'] . '.service';
+
+                $out = [];
+                $result = 0;
+
+                // Remove old service before install new
+                $this->run('systemctl stop ' . $serviceName, $out);
+                $this->run('systemctl disable ' . $serviceName, $out);
+                $this->run('rm /etc/systemd/system/' . $serviceName, $out);
+                $this->run('systemctl daemon-reload', $out);
+                $this->run('systemctl reset-failed', $out);
+
+                $content = '[Unit]' . PHP_EOL;
+                
+                if (isset($service['description']))
+                    $content .= 'Description=' . $service['description'] . PHP_EOL;
+                else
+                    $content .= 'Description=Automation service for ' . $service['name'] . PHP_EOL;
+                    
+                if (isset($service['exec-only-after']))
+                    $content .= 'After=' . $service['exec-only-after'] . PHP_EOL; // network.target
+
+                $content .= PHP_EOL . '[Service]' . PHP_EOL;
+                $content .= 'ExecStart=' . $this->pbin . '/' . $service['bin'] . PHP_EOL;
+                //$content .= 'Alias=' . $serviceName . PHP_EOL;
+
+                $content .= PHP_EOL . '[Install]' . PHP_EOL;
+                $content .= 'WantedBy=multi-user.target' . PHP_EOL;
+                //$content .= 'KillSignal=SIGTERM' . PHP_EOL;
+                //$content .= 'SendSIGKILL=no' . PHP_EOL; // Don't want to see an automated SIGKILL ever
+
+                //$content .= 'Restart=on-abort' . PHP_EOL;
+                //$content .= 'RestartSec=5s' . PHP_EOL;
+
+                $serviceFile = '/etc/systemd/system/' . $serviceName;
+                if (!\file_put_contents($serviceFile, $content) ||
+                    !\chmod($serviceFile, 0644) ||
+                    $this->run('systemctl enable ' . $serviceName, $out) !== 0) 
+                        throw new \Exception("Error while creating service '$serviceName'", 1);
+
+                $this->outSystem->stdout("\t- $serviceName", OutSystem::LEVEL_NOTICE);
+                $result = 'Done.';
+            }
+        }
+
+        $this->outSystem->stdout($result, OutSystem::LEVEL_NOTICE);
+    }
+
+    private function run(string $cmd, array &$out): int
+    {
+        $out = [];
+        $result = 0;
+        \exec($cmd, $out, $result);
+    
+        return $result;
+    }
+}
---
+++ src/InstallInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BrunoNatali\Install;
+
+interface InstallInterface
+{
+    const INSTALL_WRONG_PBIN = 0x10; // "Provide bin folder or setDir() first"
+    const INSTALL_PBIN_ERROR = 0x11; // "pbin folder does not exist on: " . $this->pbin
+    const INSTALL_WRONG_INS = 0x12; // "Instructions must be array or .json file path"
+    const INSTALL_INS_ERROR = 0x13; // "Instructions file not found in: " . $instructions
+    const INSTALL_INS_WRONG_FORMAT = 0x14; // "Instructions file in wrong format"
+    const INSTALL_DIR_ERROR = 0x16; // "App name could not be determined before set dir"
+    const INSTALL_SYS_BIN_WRONG_FORMAT = 0x17; // "'sys-bin-files' must be array"
+    const INSTALL_SYMLINK_ERROR = 0x18; // "Error while installing '$bin': " . (\is_array($out) ? implode(PHP_EOL, $out) : $out)
+    const INSTALL_SERVICE_WRONG_FORMAT = 0x19; // "'service' must be array"
+    const INSTALL_SERVICE_CONFIG_ERROR = 0x20; // "Config error in service index $key"
+
+    public function setDir(string $dir);
+
+    public function setPBin(string $pbin = null);
+
+    public function setInstructions($instructions = false);
+
+    public function getAppName(): string;
+
+    public function install();
+}
